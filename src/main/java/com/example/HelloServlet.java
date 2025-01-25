ff3fa8a3675914d90a817f4b444f39cfeaefae27
maheswarreddyv <vaddireddymaheswarreddy@gmail.com>
2025-01-25 15:45:56 +0530

    modified

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -1,16 +1,15 @@
-package com.example;
+package com.devops;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
 public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, DevOps World!</h1>");
+        out.println("<html><body>");
+        out.println("<h2>Hello, DevOps World!</h2>");
+        out.println("</body></html>");
     }
 }
