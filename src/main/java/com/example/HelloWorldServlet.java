e2e0a61e7c35dff1557737a8aa57e8bc0f8a6ebd
maheswarreddyv <vaddireddymaheswarreddy@gmail.com>
2025-01-25 16:03:56 +0530

    modified

--- src/main/java/com/example/HelloWorldServlet.java
+++ src/main/java/com/example/HelloWorldServlet.java
@@ -1,17 +1,19 @@
-package com.devops;
+package com.example;
 
+import java.io.IOException;
+import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @WebServlet("/hello")
-public class HelloWorldServlet extends HttpServlet {
+public class HelloWorldServlet extends HttpServlet implements Servlet {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().write("<h1>Welcome to the DevOps Web Application!</h1>");
+        response.getWriter().write("<h1>Hello, World! Welcome to the DevOps Practice Web App!</h1>");
     }
 }
